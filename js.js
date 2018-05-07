2d0d04a5c82022cf37a6f225e01fc2f66e933ad1
Danielz1N <silva.daniel86@Gmail.com>
2018-05-06 23:37:29 -0300

    update js
    array que pega os dados inseridos no input

--- js.js
+++ js.js
@@ -9,5 +9,13 @@ app.controller('pautaController', function($scope)
 		$scope.inputs.push({index:$scope.atualIndex,val:''});
 		
 	}
+//ainda falta o acesso ao banco de dados. Estou fazendo com PDO
+var arr=[];
+for(index in $scope.inputs){
+	if($scope.inputs[index].val.length<=0)
+		continue;
+	else
+		arr.push($scope.inputs[index].val)
+}
 }	
 );
